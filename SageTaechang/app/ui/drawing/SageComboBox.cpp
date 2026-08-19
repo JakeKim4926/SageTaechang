cd2a94d8b91d42a3209d29662d08ee35cd7c5c3b
Jake_Kim <wer1915@gmail.com>
2026-08-19 20:15:39 +0900

    fix: 콤보 높이 보정을 배치 시점으로 옮긴다
    - 컨트롤이 rectEmpty로 생성되므로 폰트 설정 시점에는 필드 rect가 비어 있었고, 그 값으로 계산해 항목 높이가 오히려 커졌다
    - WM_SIZE에서 다시 맞추고 재진입을 막는다. 필드 rect가 비면 건너뛴다

--- SageTaechang/app/ui/drawing/SageComboBox.cpp
+++ SageTaechang/app/ui/drawing/SageComboBox.cpp
@@ -1,34 +1,52 @@
-﻿#include "pch.h"
+#include "pch.h"
 #include "app/ui/drawing/SageComboBox.h"
 #include "app/ui/drawing/SageUiStyle.h"
 #include "TaechangDefine.h"
 
 BEGIN_MESSAGE_MAP(CSageComboBox, CComboBox)
 	ON_WM_PAINT()
+	ON_WM_SIZE()
 	ON_MESSAGE(WM_SETFONT, &CSageComboBox::OnSetFontMessage)
 END_MESSAGE_MAP()
 
+CSageComboBox::CSageComboBox()
+	: m_bFittingField(FALSE) {
+}
+
 void CSageComboBox::ApplyFieldHeight() {
-	if (!::IsWindow(GetSafeHwnd()))
+	if (!::IsWindow(GetSafeHwnd()) || m_bFittingField)
 		return;
+
+	m_bFittingField = TRUE;
 
 	for (int nPass = 0; nPass < TAECHANG_COMBO_FIT_MAX_PASS; ++nPass) {
 		COMBOBOXINFO info = {};
 		info.cbSize = sizeof(COMBOBOXINFO);
 		if (!GetComboBoxInfo(&info))
-			return;
+			break;
+
+		int nFieldHeight = info.rcItem.bottom - info.rcItem.top;
+		if (nFieldHeight <= 0)
+			break;
 
 		int nClosedHeight = info.rcItem.bottom + info.rcItem.top;
 		int nDelta = nClosedHeight - TAECHANG_EDIT_HEIGHT;
 		if (nDelta == 0)
-			return;
+			break;
 
 		int nItemHeight = GetItemHeight(-1) - nDelta;
 		if (nItemHeight <= 0)
-			return;
+			break;
 
 		SetItemHeight(-1, nItemHeight);
 	}
+
+	m_bFittingField = FALSE;
+}
+
+void CSageComboBox::OnSize(UINT nType, int cx, int cy) {
+	CComboBox::OnSize(nType, cx, cy);
+	ApplyFieldHeight();
 }
 
 LRESULT CSageComboBox::OnSetFontMessage(WPARAM wParam, LPARAM lParam) {
--- SageTaechang/app/ui/drawing/SageComboBox.h
+++ SageTaechang/app/ui/drawing/SageComboBox.h
@@ -1,13 +1,19 @@
-﻿#pragma once
+#pragma once
 
 class CSageComboBox : public CComboBox
 {
 	DECLARE_MESSAGE_MAP()
 
 public:
+	CSageComboBox();
+
 	void ApplyFieldHeight();
 
 protected:
 	afx_msg void OnPaint();
+	afx_msg void OnSize(UINT nType, int cx, int cy);
 	afx_msg LRESULT OnSetFontMessage(WPARAM wParam, LPARAM lParam);
+
+private:
+	BOOL m_bFittingField;
 };
